a8ece1aefcbe2ad7d71143201f2bc139e5dc1163
Ingmar Heuzinkveld <ingmar.heuzinkveld@codecentric.nl>
2017-11-23 21:23:16 +0100

    Improved calculation of values

--- src/main/kotlin/DriehoekCalculator.kt
+++ src/main/kotlin/DriehoekCalculator.kt
@@ -1,23 +1,22 @@
 class DriehoekCalculator {
     fun driehoek(row: Int): List<IntArray> {
-        return when (row) {
-            0 -> listOf(intArrayOf(1))
-            else -> {
-                val newSize = row + 1
-                val lastRow = row - 1
-                val newRow = IntArray(newSize)
-                val driehoek = driehoek(lastRow)
-                var lastRead = 0;
-                driehoek.last().withIndex().forEach { (index, value) ->
-                    if (index == driehoek.lastIndex) {
-                        newRow[index+1] = 1
-                    } else {
-                        newRow[index] = lastRead + value
-                    }
-                    lastRead = value;
-                }
-                driehoek + newRow
+        var driehoek = listOf<IntArray>()
+        for (currentRow in row downTo 1 ) {
+            val rowArray = IntArray(currentRow)
+            for (column in 1..currentRow) {
+                val value = pascal(currentRow, column)
+                rowArray[column - 1] = value
             }
+            driehoek += rowArray
         }
+        return driehoek
+    }
+
+    fun pascal(row: Int, column: Int): Int {
+        if (column == 1 || column == row) {
+            return 1
+        }
+        val value = pascal(row - 1, column - 1) + pascal(row - 1, column)
+        return value
     }
 }
--- src/test/kotlin/DriehoekSpec.kt
+++ src/test/kotlin/DriehoekSpec.kt
@@ -1,29 +1,20 @@
 
-import org.jetbrains.spek.api.Spek
-import org.jetbrains.spek.api.dsl.describe
-import org.jetbrains.spek.api.dsl.it
-import org.jetbrains.spek.api.dsl.on
-import org.junit.platform.runner.JUnitPlatform
-import org.junit.runner.RunWith
+import org.junit.Test
 
-@RunWith(JUnitPlatform::class)
-object DriehoekSpec : Spek({
-    describe("a driehoekCalculator") {
+class DriehoekSpec {
+    @Test
+    fun test1() {
         val calculator = DriehoekCalculator()
-        on("driehoek_2") {
-            val driehoek : List<IntArray> = calculator.driehoek(5);
-            it("should return ((1), (1, 1), (1, 2, 1), (1, 3, 3, 1), (1, 4, 6, 4, 1), (1, 5, 10, 10, 5, 1)") {
-                assertk.assert {
-                    driehoek.size == 6
-                    driehoek.contains(intArrayOf(1))
-                    driehoek.contains(intArrayOf(1, 1))
-                    driehoek.contains(intArrayOf(1, 2, 1))
-                    driehoek.contains(intArrayOf(1, 3, 3, 1))
-                    driehoek.contains(intArrayOf(1, 4, 6, 4, 1))
-                    driehoek.contains(intArrayOf(1, 5, 10, 10, 5, 1))
-                }
-            }
+        val driehoek : List<IntArray> = calculator.driehoek(6)
+        assertk.assert {
+            driehoek.size == 6
+            driehoek.contains(intArrayOf(1))
+            driehoek.contains(intArrayOf(1, 1))
+            driehoek.contains(intArrayOf(1, 2, 1))
+            driehoek.contains(intArrayOf(1, 3, 3, 1))
+            driehoek.contains(intArrayOf(1, 4, 6, 4, 1))
+            driehoek.contains(intArrayOf(1, 5, 10, 10, 5, 1))
         }
     }
-})
+}
 
